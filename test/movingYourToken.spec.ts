6deef4d6d3af57f5427d5a871c98407931e5e608
Javier Gómez <xgomez@xgomez.com>
2021-01-07 19:19:34 +0000

    [REFACTOR] Moves the creation of the Game test instance into a beforeEach function to remove duplication.

--- test/movingYourToken.spec.ts
+++ test/movingYourToken.spec.ts
@@ -1,17 +1,20 @@
 import {Game} from '../src/Game'
 
 describe('Token can move across the Board', () => {
-    it('Token is placed in the first square when the game is started', () => {
-        let game: Game = new Game();
 
+    let game: Game;
+
+    beforeEach(() => {
+        game = new Game();
+    })
+
+    it('Token is placed in the first square when the game is started', () => {
         game.start();
 
         expect(game.getTokenPosition()).toBe(1);
     })
 
     it('Token is on the fourth square when is moved three spaces from first square', () => {
-        let game: Game = new Game();
-
         game.start();
         game.moveToken(3);
 
